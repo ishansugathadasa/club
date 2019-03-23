b9fa24d71f3da139d691de3a91db10672ccf9c20
KentSteinwall <kentsteinwall@gmail.com>
2019-03-23 06:47:40 +0530

    basic components

---
+++ backend/routes/index.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+ 
+const ctrlUser =require('../controllers/customer.controller');
+ 
+//router.post('/register', ctrlUser.register);
+ 
+module.exports = router;
