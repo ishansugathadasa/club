8efa7be8246d372187868f924b13278ba9c85e84
KentSteinwall <kentsteinwall@gmail.com>
2019-05-27 14:27:41 +0530

    delete everything

--- frontend/src/app/shared/history.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { History } from './history.model';
-
-describe('History', () => {
-  it('should create an instance', () => {
-    expect(new History()).toBeTruthy();
-  });
-});
--- frontend/src/app/shared/history.model.ts
+++
@@ -1,9 +0,0 @@
-export class History {
-    _id:string;
-    customer_name: string;
-    tel:number;
-    instructor:string;
-    date: string; 
-    time: string;
-
-}
