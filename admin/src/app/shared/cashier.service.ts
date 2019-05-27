0ed958012a74ea61f6a4afc58a16b5fb3f395bab
KentSteinwall <kentsteinwall@gmail.com>
2019-05-27 21:40:40 +0530

    update and delete for cashier

--- admin/src/app/shared/cashier.service.ts
+++ admin/src/app/shared/cashier.service.ts
@@ -10,8 +10,21 @@ export class CashierService {
   selectCashier: Cashier;
   cashier: Cashier[];
   readonly baseURL='http://localhost:3000/register_cashier';
+  readonly baseURL1='http://localhost:3000/update_cashier';
+  readonly baseURL2='http://localhost:3000/delete_cashier';
+  readonly baseURL3='http://localhost:3000/view_cashier';
   constructor(private http: HttpClient) { }
   userpostCashier(ins: Cashier){
     return this.http.post(this.baseURL,ins);
+}
+  putCashier(ins: Cashier) {
+  return this.http.put(this.baseURL1 + `/${ins._id}`, ins);
+}
+
+  deleteCashier(_id: string) {
+  return this.http.delete(this.baseURL2 + `/${_id}`);
+}
+  getCashierList(){
+  return this.http.get(this.baseURL3);
 }
 }
